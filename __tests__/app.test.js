1912c7df2f542dc53976342aa51145372ee03385
Katelinn Jones <katiejonesyo@gmail.com>
2020-12-14 15:25:53 -0800

    all passing so far

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,10 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+    it('should GET hi', async () => {
+        const res = await request(app)
+        .get('/')
+        expect(res.text).toEqual('hi');
+    });
 
 });
